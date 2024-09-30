9845c3e52ac5684cac392c1faf9f3b62b5d2589e
Guido Zuidhof <me@guido.io>
2024-09-30 15:14:06 +0200

    Add example

---
+++ example/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/friendlycaptcha/friendly-stripe-sync/stripesync"
+)
+
+// This is an example of how to use the stripesync library.
+
+func main() {
+	cfg := stripesync.Config{
+		StripeAPIKey: "<stripe-api-key>",
+		ExcludedFields: []string{
+			"customer.address",
+		},
+		Postgres: stripesync.PostgresConfig{
+			// ...
+		},
+	}
+
+	stripesync, err := stripesync.New(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	ss, err := stripesync.GetCurrentSyncState(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("The timestamp of the last synced is %s.\n", ss.LastEventTime())
+	fmt.Println("Syncing events...")
+
+	err = stripesync.SyncEvents(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Events synced successfully.")
+}
